1505a3614b2db4d37f3efb3fe2fdcbe42f798dc5
ArnabDebnath <48032356+ArnabDebnath098@users.noreply.github.com>
2021-01-28 15:19:49 +0530

    Update Blogs.js

--- src/Blogs.js
+++ src/Blogs.js
@@ -3,27 +3,11 @@ import "./Blogs.css";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { NavLink } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
 import Accordion from "@material-ui/core/Accordion";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: "100%",
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: "33.33%",
-    flexShrink: 0,
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary,
-  },
-}));
 
 export default function ControlledAccordions() {
   const [expanded, setExpanded] = React.useState(false);
